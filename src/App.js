fb719a4885119b55fbe006236e8756bcc4f74db3
unknown <rahzy24@gmail.com>
2021-08-17 21:14:47 +0100

    ui build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [calc, setCal] = useState("");
   const [result, setResult] = useState("");
+  const [colorTheme, setColorTheme] = useState("");
 
   const ops = ['/', '*', '+', '-', '.'];
 
   const updateCalc = value => {
-    if(
+    if (
       ops.includes(value) && calc === '' ||
       ops.includes(value) && ops.includes(calc.slice(-1))
     ) {
@@ -16,8 +17,8 @@ function App() {
 
     setCal(calc + value)
 
-    if(!ops.includes(value)) {
-      setResult(eval(calc + value).toString());
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toLocaleString());
     }
   }
 
@@ -26,8 +27,9 @@ function App() {
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button onClick={() => updateCalc(i.toString()
-        )} key={i}>
+        <button
+          onClick={() => updateCalc(i.toString())}
+          key={i}>
           {i}
         </button>
       )
@@ -38,42 +40,78 @@ function App() {
   }
 
   const calculate = () => {
-    setCal(eval(calc).toString())
+    if (calc) {
+      setCal(eval(calc).toLocaleString())
+    } else {
+      setCal('syntax error #4-901')
+    }
   }
 
   const deleteLast = () => {
-    if(calc == '') {
+    if (calc == '') {
       return;
     }
-
     const value = calc.slice(0, -1)
-
     setCal(value);
+  }
+
+  const reset = () => {
+    setCal('')
+    setResult('')
+  }
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('theme-color');
+    if(currentThemeColor) {
+      setColorTheme(currentThemeColor)
+    }
+  }, []);
+
+  const handleClick = (theme) => {
+    setColorTheme(theme);
+    localStorage.setItem('theme-color', theme)
   }
 
 
   return (
-    <div className="App">
+    <div className={`App ${colorTheme}`}>
+
+      <div classNam="title">
+      <h1>calc</h1>
+      
+      <div className="theme-options">
+        <div className={`${colorTheme === 'theme-white' ? 'active' : ''} `} id="theme-white" onClick={() => handleClick('theme-white')}>1</div>
+        <div className={`${colorTheme === 'theme-black' ? 'active' : ''} `} id="theme-black" onClick={() => handleClick('theme-black')}>2</div>
+        <div className={`${colorTheme === 'theme-leet' ? 'active' : ''} `} id="theme-black" onClick={() => handleClick('theme-leet')}>3</div>
+      </div>
+
+      </div>
+
+
+
+
+
       <div className="calculator">
         <div className="display">
-          {result ? <span> ({ result }) </span> : ''}&nbsp; 
+          {result ? <span> ({result}) </span> : ''}&nbsp;
           {calc || "0"}
         </div>
 
         <div className="operators">
           <button onClick={() => updateCalc('/')}>/</button>
-          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('*')}>x</button>
           <button onClick={() => updateCalc('+')}>+</button>
           <button onClick={() => updateCalc('-')}>-</button>
 
           <button onClick={deleteLast}>DEL</button>
+          <button onClick={reset}>RESET</button>
         </div>
 
         <div className="digits">
           {createDigits()}
           <button onClick={() => updateCalc('0')}>0</button>
           <button onClick={() => updateCalc('.')}>.</button>
-          <button onClick={calculate}>=</button>
+          <button className="calculate-btn" onClick={calculate}>=</button>
         </div>
 
       </div>
